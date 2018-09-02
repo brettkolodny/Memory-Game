acb178aabf9b3363cf4037eeb327d99de9f0ba8d
brettkolodny <brettkolodny@gmail.com>
2018-09-02 17:35:55 -0400

    Started

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+
 public class Driver {
 
     public static void main(String[] args) {
-	
+        JFrame window = new JFrame("Memory Game");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        window.add(panel);
+
+        JButton startGame = new JButton("Start Game");
+        panel.add(startGame);
+
+        window.pack();
+
+        window.setVisible(true);
     }
 }
