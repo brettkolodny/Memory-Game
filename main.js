a0d510774c0d36e47d096ad5cb219e338de9d8c6
brettkolodny <brettkolodny@gmail.com>
2018-10-29 21:43:24 -0400

    Added create deck button
    Prompts for a deck nam and creates a directory for that deck if it does not already exist. Alerts the user otherwise.

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
 const path = require("path");
 
 let main_menu;
 
-createDeckBtn.addEventListener("click", event => {
-  alert("Pressed");
-});
-
 function open_main_menu() {
-  main_menu = new BrowserWindow({ width: 400, height: 600 });
-  main_menu.loadFile("main_menu.html");
-  main_menu.on("close", () => {
-    main_menu = app.exit;
+  main_menu = new BrowserWindow({
+    width: 400,
+    height: 600,
+    title: "Create Deck"
   });
-  const createDeckBtn = document.getElementById("create-deck");
+
+  main_menu.loadFile("main_menu.html");
+
+  main_menu.on("close", () => {
+    main_menu = app.exit();
+  });
 }
+
+ipcMain.on("deck_name:create", (event, deck_name) => {
+  const decks_path = path.join(__dirname, "decks", deck_name);
+  fs.mkdir(decks_path, { recursive: true }, error => {
+    if (error) {
+      if (error.code === "EEXIST") {
+        event.sender.send("alert", "Deck already exists");
+      } else {
+        event.sender.send("alert", error.message);
+      }
+    } else {
+      event.sender.getOwnerBrowserWindow().close();
+    }
+  });
+});
 
 app.on("ready", open_main_menu);
